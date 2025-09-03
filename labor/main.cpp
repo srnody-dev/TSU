a8e22b49ed1f5f96ccd415c5d6fb47260844090e
srnody-dev <smirnovdmitryyy@mail.ru>
2025-09-03 22:39:18 +0700

    Initial Commit

---
+++ labor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  labor
+//
+//  Created by srnody on 03.09.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
