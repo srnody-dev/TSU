a4d352324f973349085f335027ee95f49e6bbc81
srnody-dev <smirnovdmitryyy@mail.ru>
2025-10-07 09:56:47 +0700

    Bug fixing related to staged 2.0

--- oop1/oop1/line.hpp
+++ oop1/oop1/line.hpp
@@ -5,9 +5,39 @@
 //  Created by srnody on 07.10.2025.
 //
 
-#ifndef line_hpp
-#define line_hpp
+#pragma once
 
-#include <stdio.h>
+#include <iostream>
+#include <cmath>
 
-#endif /* line_hpp */
+class Line {
+private:
+    double a, b, c;
+    
+public:
+    
+    Line(double a_val= 0 , double b_val = 1, double c_val= 0);
+    
+    void input();
+    void output() const;
+    
+    double getA() const;
+    double getB() const;
+    double getC() const;
+    
+    void setA(double new_a);
+    void setB(double new_b);
+    void setC(double new_c);
+    
+    bool isStart() const;
+    bool isPerpendicular() const;
+    void printSlopeInfo() const;
+    bool operator || (const Line& other) const;
+    bool isPerpendicularTo(const Line& other) const;
+    double distance() const;
+    bool belongs(double x0, double y0) const;
+    double spiceCornerTo(const Line& other) const;
+};
+
+void testLineClass();
+
