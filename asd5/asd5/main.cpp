88fdfe07b386eff425e8c0a2a6ef1c51e0c3ac3c
srnody-dev <smirnovdmitryyy@mail.ru>
2025-11-08 16:24:06 +0700

    Added selection pivots:first,last,middle,median

--- asd5/asd5/main.cpp
+++ asd5/asd5/main.cpp
@@ -27,15 +27,7 @@ void swapValues(int& a, int& b) {
     b = temp;
 }
 
-int partitionArray(int arr[], int startIndex, int endIndex) {
-    int middleIndex = startIndex + (endIndex - startIndex) / 2;
-    int pivotValue = findMedian(arr[startIndex], arr[middleIndex], arr[endIndex]);
-    
-    int pivotIndex;
-    if (pivotValue == arr[startIndex]) pivotIndex = startIndex;
-    else if (pivotValue == arr[middleIndex]) pivotIndex = middleIndex;
-    else pivotIndex = endIndex;
-    
+int partition(int arr[], int startIndex, int endIndex, int pivotIndex) {
     swapValues(arr[pivotIndex], arr[endIndex]);
     
     int pivot = arr[endIndex];
@@ -47,16 +39,66 @@ int partitionArray(int arr[], int startIndex, int endIndex) {
             swapValues(arr[leftBorder], arr[currentIndex]);
         }
     }
-    
     swapValues(arr[leftBorder + 1], arr[endIndex]);
     return leftBorder + 1;
 }
 
-void quickSort(int arr[], int startIndex, int endIndex) {
+int selectFirst(int arr[], int startIndex, int endIndex) {
+    int pivotIndex = startIndex;
+    return partition(arr, startIndex, endIndex, pivotIndex);
+}
+
+int selectMiddle(int arr[], int startIndex, int endIndex) {
+    int pivotIndex = startIndex + (endIndex - startIndex) / 2;
+    return partition(arr, startIndex, endIndex, pivotIndex);
+}
+
+int selectLast(int arr[], int startIndex, int endIndex) {
+    int pivotIndex = endIndex;
+    return partition(arr, startIndex, endIndex, pivotIndex);
+}
+
+int selectMedian(int arr[], int startIndex, int endIndex) {
+    int middleIndex = startIndex + (endIndex - startIndex) / 2;
+    int pivotValue = findMedian(arr[startIndex], arr[middleIndex], arr[endIndex]);
+    
+    int pivotIndex;
+    if (pivotValue == arr[startIndex]) pivotIndex = startIndex;
+    else if (pivotValue == arr[middleIndex]) pivotIndex = middleIndex;
+    else pivotIndex = endIndex;
+    
+    return partition(arr, startIndex, endIndex, pivotIndex);
+}
+
+void quickSortFirst(int arr[], int startIndex, int endIndex) {
     if (startIndex < endIndex) {
-        int partitionIndex = partitionArray(arr, startIndex, endIndex);
-        quickSort(arr, startIndex, partitionIndex - 1);
-        quickSort(arr, partitionIndex + 1, endIndex);
+        int partitionIndex = selectFirst(arr, startIndex, endIndex);
+        quickSortFirst(arr, startIndex, partitionIndex - 1);
+        quickSortFirst(arr, partitionIndex + 1, endIndex);
+    }
+}
+
+void quickSortMiddle(int arr[], int startIndex, int endIndex) {
+    if (startIndex < endIndex) {
+        int partitionIndex = selectMiddle(arr, startIndex, endIndex);
+        quickSortMiddle(arr, startIndex, partitionIndex - 1);
+        quickSortMiddle(arr, partitionIndex + 1, endIndex);
+    }
+}
+
+void quickSortLast(int arr[], int startIndex, int endIndex) {
+    if (startIndex < endIndex) {
+        int partitionIndex = selectLast(arr, startIndex, endIndex);
+        quickSortLast(arr, startIndex, partitionIndex - 1);
+        quickSortLast(arr, partitionIndex + 1, endIndex);
+    }
+}
+
+void quickSortMedian(int arr[], int startIndex, int endIndex) {
+    if (startIndex < endIndex) {
+        int partitionIndex = selectMedian(arr, startIndex, endIndex);
+        quickSortMedian(arr, startIndex, partitionIndex - 1);
+        quickSortMedian(arr, partitionIndex + 1, endIndex);
     }
 }
 
@@ -87,16 +129,38 @@ void randomArray(int arr[], int n, int minValue, int maxValue) {
 }
 
 void testSorting() {
-    cout << "Массив 10000 эл" << endl;
-    int arr2[10000];
-    randomArray(arr2, 10000, -99999, 99999);
-    cout << "До сортировки: ";
-    printArray(arr2, 100);
+    cout << "Массивы 10000 эл" << endl;
     
-    quickSort(arr2, 0, 9999);
-    cout << "После сортировки: ";
-    printArray(arr2, 100);
-    cout << "Отсортирован: " << (isSorted(arr2, 10000) ? "Да" : "Нет") << endl;
+    int arr1[10000];
+    randomArray(arr1, 10000,-1000,1000);
+    cout << "До сортировки: ";
+    printArray(arr1, 10);
+    quickSortLast(arr1, 0, 9999);
+    cout << "Отсортирован quickSortLast: " << (isSorted(arr1, 10000) ? "Да" : "Нет") << endl;
+    
+    
+    int arr2[10000];
+    randomArray(arr2, 10000,-1000,1000);
+    cout << "До сортировки: ";
+    printArray(arr2, 10);
+    quickSortMiddle(arr2, 0, 9999);
+    cout << "Отсортирован quickSortMiddle: " << (isSorted(arr2, 10000) ? "Да" : "Нет") << endl;
+    
+    
+    int arr3[10000];
+    randomArray(arr3, 10000,-1000,1000);
+    cout << "До сортировки: ";
+    printArray(arr3, 10);
+    quickSortMedian(arr3, 0, 9999);
+    cout << "Отсортирован quickSortMedian: " << (isSorted(arr3, 10000) ? "Да" : "Нет") << endl;
+    
+    
+    int arr4[10000];
+    randomArray(arr4, 10000,-1000,1000);
+    cout << "До сортировки: ";
+    printArray(arr4, 10);
+    quickSortFirst(arr4, 0, 9999);
+    cout << "Отсортирован quickSortFirst: " << (isSorted(arr4, 10000) ? "Да" : "Нет") << endl;
 
 }
 int main() {
