139b29488ff9368be3433ea24c7786628eb7276d
srnody-dev <smirnovdmitryyy@mail.ru>
2025-12-18 09:56:49 +0700

    Added findAllIndex

--- asd2/asd2/main.cpp
+++ asd2/asd2/main.cpp
@@ -4,7 +4,6 @@
 //
 //  Created by srnody on 16.12.2025.
 //
-
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -13,7 +12,7 @@
 static const int ALPHABET_SIZE = 256;
 
 void buildBadTable(const std::string& pattern, std::vector<int>& badChar) {
-    int m = pattern.length();
+    int m = static_cast<int>(pattern.length());
     badChar.assign(ALPHABET_SIZE, m);
     
     for (int i = 0; i < m - 1; ++i) {
@@ -25,13 +24,14 @@ int findFirstIndex(const std::string& text, const std::string& pattern) {
     size_t n = text.size();
     size_t m = pattern.size();
 
-    if (m == 0) return 0;
-    if (m > n) return -1;
+    if (m == 0 || m > n) {
+        return -1;
+    }
 
     std::vector<int> badChar;
     buildBadTable(pattern, badChar);
 
-    int shift = 0;
+    size_t shift = 0;
     while (shift <= static_cast<int>(n - m)) {
         int j = static_cast<int>(m) - 1;
 
@@ -40,15 +40,61 @@ int findFirstIndex(const std::string& text, const std::string& pattern) {
         }
 
         if (j < 0) {
-            return shift;
+            return static_cast<int>(shift);
         }
 
         unsigned char mismatchChar = static_cast<unsigned char>(text[shift + j]);
         int badCharShift = badChar[mismatchChar] - (static_cast<int>(m) - 1 - j);
-        shift += std::max(1, badCharShift);
+        if (badCharShift < 1) {
+            shift += 1;
+        } else {
+            shift += badCharShift;
+        }
     }
 
     return -1;
+}
+
+std::vector<int> findAllIndex(const std::string& text, const std::string& pattern) {
+    std::vector<int> result;
+
+    size_t n = text.size();
+    size_t m = pattern.size();
+
+    if (m == 0 || m > n) {
+        return {};
+    }
+
+    std::vector<int> badChar;
+    buildBadTable(pattern, badChar);
+
+    size_t shift = 0;
+    while (shift <= static_cast<int>(n - m)) {
+        int j = static_cast<int>(m) - 1;
+
+        while (j >= 0 && pattern[j] == text[shift + j]) {
+            --j;
+        }
+
+        if (j < 0) {
+            result.push_back(static_cast<int>(shift));
+            shift += 1;
+        } else {
+            unsigned char mismatchChar =
+                static_cast<unsigned char>(text[shift + j]);
+
+            int badCharShift =
+                badChar[mismatchChar] - (static_cast<int>(m) - 1 - j);
+
+            if (badCharShift < 1) {
+                shift += 1;
+            } else {
+                shift += badCharShift;
+            }
+        }
+    }
+
+    return result;
 }
 
 
@@ -56,9 +102,18 @@ int main() {
     std::string text = "std::move_iterator is an iterator adaptor which behaves exactly like the underlying iterator";
     std::string pattern = "tor";
     
-    std::cout << "поиск 'tor'" << std::endl;
+    std::cout << "Text " << text<< std::endl;
+    std::cout << "Search  " <<pattern << std::endl;
     int result = findFirstIndex(text, pattern);
-    std::cout << "Результат: " << result << " (ожидается: 15)" << std::endl;
+    std::cout << "Результат первого вхождения: " << result << " (ожидается: 15)" << std::endl;
+    
+    std::vector<int> resultAll = findAllIndex(text, pattern);
+    std::cout << "Результат всех вхождений: ";
+    for (size_t i = 0; i < resultAll.size(); ++i) {
+        std::cout << resultAll[i];
+        std::cout << ", ";
+    }
+    std::cout << "(ожидается: 15, 30, 38, 89)" << std::endl;
     
     return 0;
 }
