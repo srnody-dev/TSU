188b375e557b19eb21c70efd030ba49d82828c78
srnody-dev <smirnovdmitryyy@mail.ru>
2026-02-16 11:47:05 +0700

    Started working on lab2_2

---
+++ asd2_2/asd2_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  asd2_2
+//
+//  Created by srnody on 11.02.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
