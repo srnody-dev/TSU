d1915824165dbb8e857e6385786526d864dc4494
srnody-dev <smirnovdmitryyy@mail.ru>
2026-02-23 21:05:46 +0700

    Configurated .gitignore

---
+++ asd2_3/asd2_3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  asd2_3
+//
+//  Created by srnody on 23.02.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
