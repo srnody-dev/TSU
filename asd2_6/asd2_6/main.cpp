9f3376611cf7c820acff8badb11939c46d74ba92
srnody-dev <smirnovdmitryyy@mail.ru>
2026-04-20 12:57:56 +0700

    test

--- asd2_6/asd2_6/main.cpp
+++ asd2_6/asd2_6/main.cpp
@@ -13,6 +13,8 @@
 
 #include "hashtable.h"
 
+/*
+
 class HashTableWindow : public QWidget {
     Q_OBJECT
 
@@ -75,14 +77,80 @@ private:
     }
 };
 
+*/
+
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    
+    /*QApplication app(argc, argv);
 
     HashTableWindow window;
     window.show();
 
     return app.exec();
+*/
+     HashFunc3 hashFunc;
+    HashTable table(&hashFunc, 5);
+    cout << "create table size 5 " << endl;
+    table.print();
+    cout << endl;
+  
+    table.insert(10, "Bently");
+    table.insert(20, "BMW");
+    table.insert(30, "Mecredes");
+    table.print();
+    cout << endl;
+  
+    table.insert(15, "Ferrari");
+    table.insert(25, "Renault");
+    table.print();
+    cout << endl;
+    
+    table.insert(10, "Maserati");
+    cout << "Update  10: Bently -> Maserati" << endl;
+    table.print();
+    cout << endl;
+    
+    cout << "contains(10): " << (table.contains(10) ? "ДА" : "НЕТ") << endl;
+    cout << "contains(20): " << (table.contains(20) ? "ДА" : "НЕТ") << endl;
+    cout << "contains(99): " << (table.contains(99) ? "ДА" : "НЕТ") << endl;
+    cout << endl;
+  
+    table.remove(20);
+    cout << "Delete 20" << endl;
+    table.print();
+    cout << endl;
+    
+    table.remove(15);
+    cout << "Delete 15" << endl;
+    table.print();
+    cout << endl;
+    
+    cout << "Текущий размер: 5" << endl;
+    table.resize(10);
+    cout << "Изменен размер на 10" << endl;
+    table.print();
+    cout << endl;
+  
+    HashFunc2 hashFunc2;
+    HashTable table3(&hashFunc2, 3);
+    table3.insert(1, "One");
+    table3.insert(2, "Two");
+    cout << "Таблица 3 до swap:" << endl;
+    table3.print();
+    
+    table.swap(table3);
+    cout << "После swap(table, table3):" << endl;
+    cout << "Таблица (бывшая table3):" << endl;
+    table.print();
+    cout << "Таблица 3 (бывшая table):" << endl;
+    table3.print();
+    cout << endl;
+    
+    cout << "swap HashFunc3 на HashFunc2" << endl;
+    table.changeHashFunc(&hashFunc2);
+    table.print();
+    cout << endl;
 }
 
-#include "main.moc"
+//#include "main.moc"
